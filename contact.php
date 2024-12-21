6b946b2306d013191403af9f450c7a2f8b51b147
pradumbh kourav <pradumbh.kourav.tec@gmail.com>
2024-12-21 17:57:57 +0530

    new contact

---
+++ contact.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+    
+    // Set email details
+    $to = "pradumbh.kourav@gmail.com"; // Your email address
+    $subject = "New Contact Form Submission: " . $subject;
+    $body = "You have received a new message from the contact form.\n\n";
+    $body .= "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Subject: $subject\n";
+    $body .= "Message: $message\n";
+    
+    // Headers for email
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again.";
+    }
+}
+?>
